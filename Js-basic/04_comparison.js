09242bc94dfed3723452f69c3ba348fdd0bdb889
Akash4056 <akashsaha1313@gmail.com>
2024-01-03 23:29:15 +0530

    comparison done

---
+++ Js-basic/04_comparison.js
@@ -0,0 +1,29 @@
+// normal comparison b/w num
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// moderate comparison b/w string and number
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null);
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+console.log(undefined);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined>=0);
+
+// strict check '==='
+
+console.log("2"===2);
+console.log("2"==="2");
