80fde7714661723059c2788edc7512a80ab1aefa
Akash4056 <akashsaha1313@gmail.com>
2024-01-04 01:37:00 +0530

    math and num done

---
+++ 01_basic/06_nums_math.js
@@ -0,0 +1,34 @@
+// const score = 600;
+
+// const balance = new Number(100)
+
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber=83.8966
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreds=100000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//__________ MATHS __________
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.9));
+// console.log(Math.ceil(9.9));
+// console.log(Math.floor(9.9));
+// console.log(Math.min(3,4,5,6,7));
+// console.log(Math.max(3,4,5,6,7));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
