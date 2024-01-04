4d1ac06cbd29ead6dc2998219fe727d48b870502
Akash4056 <akashsaha1313@gmail.com>
2024-01-04 19:26:46 +0530

    object

--- 02_basic/03_object.js
+++ 02_basic/03_object.js
@@ -2,4 +2,39 @@
 
 //object literals
 
-const JsUser={}
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "akash",
+    age: 18,
+    "full name": "Akash Saha",
+    [mySym]: "key1",
+    location: "bhubaneswar",
+    isLoggedIn: "false",
+    email: "akash@google.com",
+    lastLoginDays: ["Monday", "Saturday"]
+    ,
+    greeting: function () {
+        console.log("Hello JS user");
+    },
+    greetingTwo: function () {
+        console.log(`Hello JS user, ${this.name}`);
+    }
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "akash@chatgpt.com"
+Object.freeze(JsUser)
+JsUser.email = "akash@microsoft.com"
+// console.log(JsUser);
+
+// console.log(JsUser.greeting);
+// console.log(JsUser.greetingTwo);
+JsUser.greeting()
+JsUser.greetingTwo()
+console.log(JsUser.greetingTwo());
+console.log(JsUser.greeting());
