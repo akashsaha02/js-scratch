2d652641189fc7a17aa91d3596d002dc6eee33ad
Akash4056 <akashsaha1313@gmail.com>
2024-01-05 20:21:19 +0530

    execution context

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,17 @@
+// Immediately Invoked Function Expressions(IIFE)
+
+// (function chai() {
+//     console.log(`DB CONNECTED`);
+// })();
+
+// ((name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// })("AKASH");
+
+let val1=10
+let val2=5
+
+function addSum(n1,n2){
+    return n1+n2;
+}
+
