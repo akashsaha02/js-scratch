cba4b3ee118d342a59dadd26812f141ebba7a068
Akash4056 <akashsaha1313@gmail.com>
2024-01-06 23:25:07 +0530

    loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    const element = myObject[key];
+    console.log(element);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
